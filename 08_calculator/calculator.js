45e5534d3e72029b573dd2381209e38703fb9933
thomasalabaster <tomalabaster@gmail.com>
2023-03-16 16:21:56 -0700

    Javascript fundamentals

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,55 @@
-const add = function() {
-	
-};
+const add = (a,b) => a + b;
 
-const subtract = function() {
-	
-};
+const subtract = (a, b) => a - b;
 
 const sum = function() {
-	
+  let newSum = 0;
+  if (arguments.length === 0)
+  {
+    return 0;
+  }
+  for (let i = 0; i < arguments[0].length; i++)
+  {
+    newSum += parseInt(arguments[0][i]);
+  }
+  return newSum;
 };
 
 const multiply = function() {
-
+  let newSum = 0;
+  if (arguments.length === 0)
+  {
+    return 0;
+  }
+  newSum = arguments[0][0];
+  for (let i = 1; i < arguments[0].length; i++)
+  { 
+    newSum *= arguments[0][i];
+  }
+  return newSum;
+  
 };
 
-const power = function() {
-	
+const power = function(num, power) {
+  let finalNum = num;
+  for (let i = 1; i < power; i++)
+  {
+    finalNum *= num;
+  }
+  return finalNum;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let newFactorial = 1;
+  if (num === 0)
+  {
+    return 1;
+  }
+  for (let i = 1; i <= num; i++)
+  {
+    newFactorial *= i;
+  }
+  return newFactorial;
 };
 
 // Do not edit below this line
